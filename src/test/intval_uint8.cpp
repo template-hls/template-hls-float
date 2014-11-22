4d6b64b0bbbbb03038d383b74c3418272a9501a3
m8pple <dt10@imperial.ac.uk>
2014-11-22 16:19:02 +0000

    Rename

--- src/test/intval_uint8.cpp
+++ src/test/intval_uint8.cpp
@@ -2,8 +2,8 @@
 
 int64_t add_ival_uint8(int64_t _a, int64_t _b)
 {
-	auto a=intval<0,127> a(_a);
-	auto b=intval<0,127> b(_b);
+	auto a=make_intval<0,255>(_a);
+	auto b=make_intval<0,255>(_b);
 	
 	return (a+b).val;
 }
